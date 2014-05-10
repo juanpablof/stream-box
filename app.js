9de4365535087ab9e7f589b497e65eced085ad85
Juan Pablo Francisconi <juanpablof@gmail.com>
2014-05-09 23:15:14 -0400

    Initial commit (heroku integration mostly).

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express"),
+    app     = express(),
+    port    = 9999;
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); // NOT SAFE FOR PRODUCTION
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+app.use(express.static(__dirname));
+app.listen(port);
